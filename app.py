39827555f0f6e8343f0d66605a7d5eceb24f5c5f
K3nny13 <kenneth_cogzell@hotmail.com>
2018-09-11 08:38:00 +0000

    changed users.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from user import User
 
 
-my_user = User('kenny@fake.com', 'Kenny', "Cogzell", 1)
-
+my_user = User('kenny@fake.com', 'Kenny', "Cogzell", None)
+my_user.save_to_db()
 print(my_user)
--- user.py
+++ user.py
@@ -1,9 +1,22 @@
-class User:
-	def __init__(self, email, first_name, last_name, id):
-		self.email = email
-		self.first_name = first_name
-		self.last_name = last_name
-		self.id = id
+import psycopg2
+import getpass
 
-	def __repr__(self):
-		return "<User {}>".format(self.email)
+
+class User:
+    def __init__(self, id, email, first_name, last_name):
+        self.id = id
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)
+
+    def save_to_db(self):
+        connection = psycopg2.connect(user="rojppsyv", password=getpass.getpass(
+        ), database="rojppsyv", host="postgres://rojppsyv:pPn1dzV5YjY7get2UOQewnbjMJjwAGg7@dumbo.db.elephantsql.com:5432")
+        with connection.cursor() as cursor:
+            cursor.execute("INSERT INTO users (email, first_name, last_name) VALUES (%s,%s,%s)",
+                           (self.email, self, first_name, self.last_name))
+        connection.commit()
+        connection.close()
