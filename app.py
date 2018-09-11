ca1721694d5705a1e90f46ed1aadfdbf52b16d68
K3nny13 <kenneth_cogzell@hotmail.com>
2018-09-11 21:43:01 +0200

    fixed db

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from user import User
 # my_user = User(None, 'rochelle@fake.com', 'Rochelle', "Rossouw")
 # my_user.save_to_db()
 
-my_user = User.load_from_db_by_email('kenneth@fake.com')
-
+my_user = User(None, first_name="First", last_name="Last", email="Last@fake.com")
+my_user.save_to_db()
 print(my_user)
--- user.py
+++ user.py
@@ -13,15 +13,14 @@ class User:
 
     def save_to_db(self):
         with CursorFromConnectionFromPool() as cursor:
-			cursor.execute("INSERT INTO users (first_name, last_name, email) VALUES (%s,%s,%s)",
-							(self.first_name, self.last_name, self.email)) # second arguement must be a tuple
+            cursor.execute("INSERT INTO users (first_name, last_name, email) VALUES (%s,%s,%s)",
+            (self.first_name, self.last_name, self.email)) # second arguement must be a tuple
         # connection.commit() -- 'with' automatically .commits() and .close() when closed
         # connection.close()
 
     @classmethod
     def load_from_db_by_email(cls, email):
-		with CursorFromConnectionFromPool() as cursor:
-			cursor.execute('SELECT * FROM users WHERE email=%s', (email,)) # second arguement must be a tuple
-			user_data = cursor.fetchone()
-			return cls(id=user_data[0], email=user_data[3], first_name=user_data[1], last_name=user_data[2])
-
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('SELECT * FROM users WHERE email=%s', (email,)) # second arguement must be a tuple
+            user_data = cursor.fetchone()
+            return cls(id=user_data[0], email=user_data[3], first_name=user_data[1], last_name=user_data[2])
